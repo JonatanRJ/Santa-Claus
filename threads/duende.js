43ebeddad66f5ba52f765894a86cf882f02d3ccc
moises-elmoy <moises.iniguezo@alumnos.udg.mx>
2022-03-29 12:14:08 -0600

    Update duende.js

--- threads/duende.js
+++ threads/duende.js
@@ -1,16 +1,16 @@
-/* -- Declare local variables -- */
+/* -- Declaracion de las variables globales -- */
 let duendes = 0
 
-/* -- Message to elf thread -- */
+/* -- Mensaje del hilo de los elfos/duendes -- */
 process.on('message', (obj) => {
-    //add elf
+    //Sumar duende
     if(obj.message == 1){
-        //Elf less than 3
+        //Duende menor a 3
         if(duendes < 2){
             duendes++
             process.send({message:duendes})
         }
-        //Send message to Santa thread
+        //Enviar mensaje al hilo de Santa
         else{
             duendes = 0
             process.send({message:duendes, action:"regalos"})
